2f89c16c80958940703e4d4296d494e0eacb58fc
lehannK <leandro_hollies@hotmail.com>
2023-12-11 16:55:47 -0300

    commit

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <h2>Layout da página 'sobre'</h2>
+      {children}
+    </>
+  );
+}
---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "novo head title",
+};
+
+export default function About() {
+  return (
+    <>
+      <main>
+        <h1>Teste</h1>
+      </main>
+    </>
+  );
+}
